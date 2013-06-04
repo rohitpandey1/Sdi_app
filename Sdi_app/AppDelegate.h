6ffd621b7d267cb919e086d8e7b7de72b118f951
Rohit <ashu@Rohits-Mac-mini.local>
2013-06-04 12:16:42 +0530

    Initial Commit

---
+++ Sdi_app/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Sdi_app
+//
+//  Created by Rohit on 04/06/13.
+//  Copyright (c) 2013 SDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Sdi_app/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sdi_app
+//
+//  Created by Rohit on 04/06/13.
+//  Copyright (c) 2013 SDI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
